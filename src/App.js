409671b278770a58aa0b280f569c734682b806c4
Rosen Georgiev <73111903+RosenGeorgievvv@users.noreply.github.com>
2021-11-30 15:11:54 +0200

    Finished

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 import ChatFeed from './components/ChatFeed';
-
+import LoginForm from './components/LoginForm'
 
 
 const App = () => {
+        if (!localStorage.getItem('username')) return <LoginForm / >
 
-        return ( <
-            ChatEngine height = "100vh"
-            projectID = "4e7f487c-cd90-4256-acf6-3df0f726807a"
-            userName = "Rosen"
-            userSecret = "Rosen123"
-            renderChatFeed = {
-                (chatAppProps) => < ChatFeed {...chatAppProps }
-                />} /
-                >
-            )
-        }
+
+            return ( <
+                ChatEngine height = "100vh"
+                projectID = "4e7f487c-cd90-4256-acf6-3df0f726807a"
+                userName = { localStorage.getItem('username') }
+                userSecret = { localStorage.getItem('password') }
+                renderChatFeed = {
+                    (chatAppProps) => < ChatFeed {...chatAppProps }
+                    />} /
+                    >
+                )
+            }
 
         export default App;
