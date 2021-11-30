45347d3fdb7a22c0ad66731f69ac67b89413c4ec
Rosen Georgiev <73111903+RosenGeorgievvv@users.noreply.github.com>
2021-11-30 10:46:15 +0200

    ChatEngine core

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+
+
+const App = () => {
+
+    return ( <
+        ChatEngine height = "100vh"
+        projectID = "4e7f487c-cd90-4256-acf6-3df0f726807a"
+        userName = "Rosen"
+        userSecret = "Rosen123"
+
+        /
+        >
+    )
+}
+
+export default App;
