80f4e1637cbb63dea37e1187b32e91971d610e5f
Rosen Georgiev <73111903+RosenGeorgievvv@users.noreply.github.com>
2021-11-30 11:19:29 +0200

    Creating all necessary components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
+import ChatFeed from './components/ChatFeed';
+
+
 
 const App = () => {
 
-    return ( <
-        ChatEngine height = "100vh"
-        projectID = "4e7f487c-cd90-4256-acf6-3df0f726807a"
-        userName = "Rosen"
-        userSecret = "Rosen123"
+        return ( <
+            ChatEngine height = "100vh"
+            projectID = "4e7f487c-cd90-4256-acf6-3df0f726807a"
+            userName = "Rosen"
+            userSecret = "Rosen123"
+            renderChatFeed = {
+                (chatAppProps) => < ChatFeed {...chatAppProps }
+                />} /
+                >
+            )
+        }
 
-        /
-        >
-    )
-}
-
-export default App;
+        export default App;
